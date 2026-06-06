b869c114e577a9dccf4c411090a76ce85485986a
Dmitriy Gavrilov <gavrdmg@gmail.com>
2026-06-06 13:40:35 +0300

    add math operations

---
+++ src/main/java/ru/gavrdmg/Main.java
@@ -0,0 +1,26 @@
+package ru.gavrdmg;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        double c = 5.0;
+        double d = 3.0;
+        int outOfRange = Integer.MAX_VALUE + 1;
+        System.out.printf("a=%d b=%d c=%f d=%f\n", a, b, c, d);
+        System.out.printf("a-b=%d \n", a - b);
+        System.out.printf("a+b=%d \n", a + b);
+        System.out.printf("a-b=%d \n", a - b);
+        System.out.printf("a*b=%d \n", a * b);
+        System.out.printf("b/a=%d \n", b / a);
+        System.out.printf("c+a*(c-b)=%f \n", c + a * (c - b));
+        System.out.printf("a>b = %b \n", a > b);
+        System.out.printf("b<c = %b \n", b < c);
+        System.out.printf("a>b = %b \n", a > b);
+        System.out.printf("b<c = %b \n", b < c);
+        System.out.printf("b<=d = %b \n", b <= d);
+        System.out.println(outOfRange);
+    }
+}
